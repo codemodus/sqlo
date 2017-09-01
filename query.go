52775f0e34a9566275085b0092f26ca42d3a00d5
Daved <daved@codemodus.com>
2017-08-31 21:06:04 -0700

    Properly implement query interface, remove name args.

--- query.go
+++ query.go
@@ -6,72 +6,68 @@ import (
 
 // PlainQry ...
 type PlainQry struct {
-	name string
 	qry  string
 	args []interface{}
 }
 
 // NewPlainQry ...
-func (*SQLO) NewPlainQry(name, query string, args ...interface{}) *PlainQry {
-	return &PlainQry{name, query, args}
+func (*SQLO) NewPlainQry(query string, args ...interface{}) *PlainQry {
+	return &PlainQry{query, args}
 }
 
-// Query ...
-func (qry *PlainQry) Query(ctx context.Context, q Queryable) error {
+// Send ...
+func (qry *PlainQry) Send(ctx context.Context, q Queryable) error {
 	_, err := q.ExecContext(ctx, qry.qry, qry.args...)
 	return err
 }
 
 // NamedQry ...
 type NamedQry struct {
-	name string
-	qry  string
-	arg  interface{}
+	qry string
+	arg interface{}
 }
 
 // NewNamedQry ...
-func (*SQLO) NewNamedQry(name, query string, arg interface{}) *NamedQry {
-	return &NamedQry{name, query, arg}
+func (*SQLO) NewNamedQry(query string, arg interface{}) *NamedQry {
+	return &NamedQry{query, arg}
 }
 
-// Query ...
-func (qry *NamedQry) Query(ctx context.Context, q Queryable) error {
+// Send ...
+func (qry *NamedQry) Send(ctx context.Context, q Queryable) error {
 	_, err := q.NamedExecContext(ctx, qry.qry, qry.arg)
 	return err
 }
 
 // GetQry ...
 type GetQry struct {
-	name string
 	dest interface{}
 	qry  string
 	args []interface{}
 }
 
 // NewGetQry ...
-func (*SQLO) NewGetQry(name string, dest interface{}, query string, args ...interface{}) *GetQry {
-	return &GetQry{name, dest, query, args}
+func (*SQLO) NewGetQry(dest interface{}, query string, args ...interface{}) *GetQry {
+	return &GetQry{dest, query, args}
 }
 
-// Query ...
-func (qry *GetQry) Query(ctx context.Context, q Queryable) error {
+// Send ...
+func (qry *GetQry) Send(ctx context.Context, q Queryable) error {
 	return q.GetContext(ctx, qry.dest, qry.qry, qry.args...)
 }
 
 // SelectQry ...
 type SelectQry struct {
-	name string
 	dest interface{}
 	qry  string
 	args []interface{}
 }
 
 // NewSelectQry ...
-func (*SQLO) NewSelectQry(name string, dest interface{}, query string, args ...interface{}) *SelectQry {
-	return &SelectQry{name, dest, query, args}
+func (*SQLO) NewSelectQry(dest interface{}, query string, args ...interface{}) *SelectQry {
+	return &SelectQry{dest, query, args}
 }
 
-// Query ...
-func (qry *SelectQry) Query(ctx context.Context, q Queryable) error {
+// Send ...
+func (qry *SelectQry) Send(ctx context.Context, q Queryable) error {
 	return q.SelectContext(ctx, qry.dest, qry.qry, qry.args...)
 }
